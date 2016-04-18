2b3030a1a2f7ff4e136b9af85197aabeb51a95b4
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-04-18 12:08:37 +0900

    Separate docker image build job and openresty build job

--- build-openresty/run.sh
+++
@@ -1,3 +0,0 @@
-fly sp -t sample -c pipeline.yml -p build-openresty -l credentials.yml -n
-fly up -t sample -p build-openresty
-curl http://192.168.100.4:8080/pipelines/build-openresty/jobs/job-publish-docker-image-building-openresty -X POST
---
+++ docker-images/openresty-builder/run.sh
@@ -0,0 +1,2 @@
+fly sp -t sample -c pipeline.yml -p openresty-builder vag-l credentials.yml -n
+fly up -t sample -p build-openresty
