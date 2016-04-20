47c8f1604888632dc566fd5d2fe18bf445b2c7d4
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-04-20 15:41:18 +0900

    Generate an RPM package of OpenResty in a docker container

--- build-openresty/build.sh
+++ build-openresty/build.sh
@@ -3,4 +3,14 @@
 set -e
 set -x
 
-nginx-build -d work -openresty -pcre -openssl -zlib
+nginx-build -d work -openresty -openrestyversion=1.9.7.4 -pcre -openssl -zlib
+cd work/openresty/1.9.7.4/openresty-1.9.7.4
+make install DESTDIR=/tmp/openresty
+fpm -s dir -t rpm \
+  -v 1.9.7.4 \
+  -n openresty \
+  -C /tmp/openresty \
+  -a x86_64 \
+  --description "OpenResty v1.9.7.4" \
+  -p openresty-1.9.7.4.rpm \
+  .
