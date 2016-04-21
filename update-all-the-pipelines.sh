074dae1d890fff07753d1033b499144ea9a85cc9
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-04-21 12:38:39 +0900

    Restructure the architecture of this repository

---
+++ update-all-the-pipelines.sh
@@ -0,0 +1,7 @@
+# Update docker-images pipeline.
+fly sp -t sample -c pipelines/openresty-builder.yml -p docker-images -l config/credentials.yml -n
+fly up -t sample -p docker-images
+
+# Update build-openresty pipeline.
+fly sp -t sample -c pipelines/build-openresty.yml -p build-openresty -l config/credentials.yml -n
+fly up -t sample -p build-openresty
