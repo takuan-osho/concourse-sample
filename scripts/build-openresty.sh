fcb6b1a0ab00f81d201041ded63ff4c5c07fe210
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-04-21 18:38:04 +0900

    Separate OpenResty version information from a task shell script

--- scripts/build-openresty.sh
+++ scripts/build-openresty.sh
@@ -3,14 +3,19 @@
 set -e
 set -x
 
-nginx-build -d work -openresty -openrestyversion=1.9.7.4 -pcre -openssl -zlib
-cd work/openresty/1.9.7.4/openresty-1.9.7.4
+nginx-build -d work \
+  -openresty \
+  -openrestyversion=${OPENRESTY_VERSION} \
+  -pcre \
+  -openssl \
+  -zlib
+cd work/openresty/${OPENRESTY_VERSION}/openresty-${OPENRESTY_VERSION}
 make install DESTDIR=/tmp/openresty
 fpm -s dir -t rpm \
-  -v 1.9.7.4 \
+  -v ${OPENRESTY_VERSION} \
   -n openresty \
   -C /tmp/openresty \
   -a x86_64 \
-  --description "OpenResty v1.9.7.4" \
-  -p openresty-1.9.7.4.rpm \
+  --description "OpenResty" \
+  -p openresty-${OPENRESTY_VERSION}.rpm \
   .
