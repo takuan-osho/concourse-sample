56b15b096304c157424f0b27cafb9c22c7c9d5ad
SHIMIZU Taku <shimizu.taku@gmail.com>
2016-04-25 14:25:11 +0900

    Separate version numbers of libraries from task yams files

--- update-all-the-pipelines.sh
+++ update-all-the-pipelines.sh
@@ -5,5 +5,5 @@ fly sp -t sample -c pipelines/openresty-builder.yml -p docker-images -l config/c
 fly up -t sample -p docker-images
 
 # Update build-openresty pipeline.
-fly sp -t sample -c pipelines/build-openresty.yml -p build-openresty -l config/credentials.yml -n
+fly sp -t sample -c pipelines/build-openresty.yml -p build-openresty -l config/credentials.yml -l config/versions.yml -n
 fly up -t sample -p build-openresty
